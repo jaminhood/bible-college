3b7fefd44523d5d548bbac78fc8dfa2c6cfb54a6
syntax007 <m.ogundipe@amorservsolutions.com>
2023-05-19 16:27:42 +0100

    from Michael

--- src/redux/usersSlice.jsx
+++ src/redux/usersSlice.jsx
@@ -1,60 +1,53 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { getStorage, setStorage } from "../helpers";
 
-
 const initialState = {
   student: getStorage(`students`),
-  admin: [
-    { id: 1, username: `jamin`, password: `jamin` }
-  ],
-  active: getStorage(`active`)
-}
+  admin: [{ id: 1, username: `admin`, password: `admin` }],
+  active: getStorage(`active`),
+};
 
 const usersSlice = createSlice({
   name: `users`,
   initialState,
   reducers: {
     studentLogin: {
-      reducer: (state, action) =>
-      {
-        state.active = action.payload
-        setStorage(`active`, state.active)
+      reducer: (state, action) => {
+        state.active = action.payload;
+        setStorage(`active`, state.active);
       },
-      prepare: (name, matricNumber) =>
-      {
+      prepare: (name, matricNumber) => {
         return {
-          payload: {name, matricNumber, role: `student` }
-        }
-      }
+          payload: { name, matricNumber, role: `student` },
+        };
+      },
     },
     adminLogin: {
-      reducer: (state, action) =>
-      {
-        const { username, password } = action.payload
-        const currentUser = state.admin.find(user => user.username === username)
-        if (currentUser && currentUser.password === password)
-        {
-          state.active = { id: nanoid(), ...action.payload, role: `admin` }
-          setStorage(`active`, state.active)
+      reducer: (state, action) => {
+        const { username, password } = action.payload;
+        const currentUser = state.admin.find(
+          (user) => user.username === username
+        );
+        if (currentUser && currentUser.password === password) {
+          state.active = { id: nanoid(), ...action.payload, role: `admin` };
+          setStorage(`active`, state.active);
         }
       },
-      prepare: (username, password) =>
-      {
+      prepare: (username, password) => {
         return {
-          payload: { username, password }
-        }
-      }
+          payload: { username, password },
+        };
+      },
     },
-    logout: (state) =>
-    {
-      state.active = []
-      setStorage(`active`, state.active)
-    }
-  }
-})
+    logout: (state) => {
+      state.active = [];
+      setStorage(`active`, state.active);
+    },
+  },
+});
 
-export const activeUser = state => state.user.active
-export const students = state => state.user.student
-export const { studentLogin, adminLogin, logout } = usersSlice.actions
+export const activeUser = (state) => state.user.active;
+export const students = (state) => state.user.student;
+export const { studentLogin, adminLogin, logout } = usersSlice.actions;
 
-export default usersSlice.reducer
+export default usersSlice.reducer;
